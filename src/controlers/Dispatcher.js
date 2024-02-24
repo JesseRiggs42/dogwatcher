4ab6435b745ba98235666a311a6de3af17596e0d
Jesse Riggs <jesseriggs@u.boisestate.edu>
2024-02-24 10:54:15 -0700

    sketching out logic for dispatcher

--- src/controlers/Dispatcher.js
+++ src/controlers/Dispatcher.js
@@ -1,23 +1,41 @@
+import BoardFactory from "../models/BoardFactory";
 
 // TODO:Test this file.
-const MODELS = {};
-const TIMESTAMPS = {
-    lastRequest : Date.now(),
-    lastResponse : Date.now()
-};
 let dispatcherInstance;
 
 export default class Dispatcher {
-    constructor() {
+    constructor(controller, fetcher, models) {
         if(dispatcherInstance) {
             throw new Error('Cannot instantiate more than one dispatcher.');
         }
+
         dispatcherInstance = this;
+        
+        this.controller = controller;
+        this.fetcher = fetcher;
+        this.models = models ?? {};
     }
 
     sendRequest(request) {
         if(request === null) {
             throw new Error('Cannot dispatch null request.');
         }
+        fetchModel(request.getModelKey());
+    }
+
+    fetchModel(modelKey) {
+
+        let model = this.models[modelKey];
+        if(model === null) {
+            let [ baseKey, classKey ] = modelKey.split(constants.KEY_DELIMITER);
+            let baseModel = this.models[baseKey];
+            if(baseModel === null) {
+                let modelHtml = this.fetcher.fetch(baseKey);
+                let parsedHtml = this.parser.parse(modelHtml);
+                model = BoardFactory.create(parsedHtml, classKey);
+            }
+            model = fetchModel(modelKey);
+        }
+
     }
 }
