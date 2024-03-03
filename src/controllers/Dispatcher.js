b7bedeec56ef67bb02958b3f032d41b50c68d784
Jesse Riggs <jesseriggs@u.boisestate.edu>
2024-03-03 10:19:09 -0700

    Stub out idea for dispatcher

--- src/controllers/Dispatcher.js
+++ src/controllers/Dispatcher.js
@@ -1,33 +1,38 @@
 import BoardFactory from "../models/BoardFactory";
 
-// TODO:Test this file.
+// TODO: Finish this class. Won't run, haven't tried.
+// TODO: Test this file.
 let dispatcherInstance;
 
 export default class Dispatcher {
-    constructor(controller, fetcher, models) {
+    constructor(controller, fetcher, defaultModelKey, models) {
         if(dispatcherInstance) {
             throw new Error('Cannot instantiate more than one dispatcher.');
         }
 
         dispatcherInstance = this;
-        
+
         this.controller = controller;
         this.fetcher = fetcher;
         this.models = models ?? {};
+        this.defaultModelKey = defaultModelKey;
+        this.currentModelKey = defaultModelKey;
+
+        isValidDispatcher();
     }
 
     sendRequest(request) {
         if(request === null) {
             throw new Error('Cannot dispatch null request.');
         }
-        fetchModel(request.getModelKey());
+        __fetch_model(request.getModelKey());
     }
 
-    fetchModel(modelKey) {
+    __fetch_model(modelKey) {
 
         let model = this.models[modelKey];
         if(model === null) {
-            let [ baseKey, classKey ] = modelKey.split(constants.KEY_DELIMITER);
+            let [ baseKey, classKey ] = modelKey.split(MODEL_KEY_DELIMITER);
             // TODO: grab either cached data or fetch new.
             // culd modelHtml be part of the modelHtml Structure?
             let modelHtml = this.fetcher.fetch(baseKey, baseKey);
